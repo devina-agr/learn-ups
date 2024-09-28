bf8b6880dd344de1bbb9c276213665ac2d583ac9
devina-agr <devinaagrawal2005@gmail.com>
2024-09-28 10:50:36 +0530

    search

---
+++ devina/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  //   {/* <App /> */}
+  //   <h3>hellooo</h3>
+  // </StrictMode>,
+  <div>
+
+     <form action="">
+    <input type="text" placeholder='Search'/>
+    </form>
+    <div>
+     
+    </div>
+  </div>
+);
